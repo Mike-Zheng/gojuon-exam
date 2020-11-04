33d3242d8d6e334c1dff5adaf8f74ee1540b961d
mikezheng <mikezheng@weintek.com>
2020-11-04 14:01:48 +0800

    [add] katakana

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <HiraganaExam />
+  <!-- <Exam :examType="'hiragana'" /> -->
+  <Exam :examType="pickedType" />
+
+  <input type="radio" id="hiragana" value="hiragana" v-model="pickedType" />
+  <label for="hiragana">hiragana</label>
+
+  <input type="radio" id="katakana" value="katakana" v-model="pickedType" />
+  <label for="katakana">katakana</label>
+  <br />
 </template>
 
 <script>
-import Hiragana from './components/Hiragana.vue'
-import HiraganaExam from './components/HiraganaExam.vue'
+import Hiragana from "./components/Hiragana.vue";
+import Exam from "./components/Exam.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Hiragana,
-    HiraganaExam
-  }
-}
+    Exam,
+  },
+  data() {
+    return {
+      pickedType: "hiragana",
+    };
+  },
+};
 </script>
--- src/components/Exam.vue
+++ src/components/Exam.vue
@@ -1,4 +1,5 @@
 <template>
+  <div style="margin-top: 20px">{{ corrected.length }}/{{ textLength }}</div>
   <div class="board horizontal">
     <div
       class="word-card"
@@ -9,7 +10,9 @@
         wrong: item.isAnsCorrect == -1,
       }"
     >
-      {{ item.hira_unicode }}
+      <span v-if="examType == 'hiragana'">{{ item.hira_unicode }}</span>
+      <span v-if="examType == 'katakana'">{{ item.kata_unicode }}</span>
+
       <span v-if="item.showAns">{{ item.roma }}</span>
       <input
         v-else
@@ -19,15 +22,13 @@
         @input="validAns($event, item, index)"
       />
     </div>
-
-    {{corrected.length}}/{{textLength}}
   </div>
 </template>
 
 <script>
 import { basic } from "../gojuon.js";
 export default {
-  name: "HiraganaExam",
+  name: "Exam",
   data() {
     return {
       gojuon: basic,
@@ -35,33 +36,57 @@ export default {
       textLength: 100,
     };
   },
-  computed:{
-    corrected(){
-      return this.textList.filter(_=>_.isAnsCorrect===1)
-    }
+  props: {
+    examType: {
+      type: String,
+      default: "hiragana",
+    },
+  },
+  watch: {
+    examType: function (newValue, rawValue) {
+      if (newValue !== rawValue) {
+        this.init();
+      }
+    },
+  },
+  computed: {
+    corrected() {
+      return this.textList.filter((_) => _.isAnsCorrect === 1);
+    },
   },
   mounted() {
-    while (this.textList.length < this.textLength) {
-      const index = this.getRandomInt(0, 50);
-      console.log(this.gojuon[index]);
-      if (this.gojuon[index].roma != "") {
-        this.textList.push({
-          ...this.gojuon[index],
-          ...{ inputValue: "", isAnsCorrect: 0, showAns: false },
-        });
-      }
-    }
+    this.init();
   },
   methods: {
+    init() {
+      this.textList = [];
+
+      while (this.textList.length < this.textLength) {
+        const index = this.getRandomInt(0, 50);
+
+        if (this.gojuon[index].roma != "") {
+          this.textList.push({
+            ...this.gojuon[index],
+            ...{ inputValue: "", isAnsCorrect: 0, showAns: false },
+          });
+        }
+      }
+
+      this.$nextTick(() => {
+        this.focus(0);
+      });
+    },
     getRandomInt(min, max) {
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1)) + min;
     },
     validAns(event, item, index) {
-
       // 放棄
-      if (item.inputValue[0] === " ") {
+      if (
+        item.inputValue[0] === " " ||
+        item.inputValue.split("").indexOf(" ") > -1
+      ) {
         item.isAnsCorrect = -1;
         item.showAns = true;
         this.focus(index + 1);
@@ -101,7 +126,7 @@ export default {
 <style>
 .board {
   display: flex;
-  margin: 30px;
+  margin: 30px auto;
   flex-wrap: wrap;
   justify-content: center;
   align-content: stretch;
